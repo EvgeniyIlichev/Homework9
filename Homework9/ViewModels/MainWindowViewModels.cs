74f956cc0009a2aa7684dc07c8233822ce580724
EvgeniyIlichev <888ilichoff@mail.ru>
2023-02-01 20:57:55 +0300

    Рефакторинг

--- Homework9/ViewModels/MainWindowViewModels.cs
+++ Homework9/ViewModels/MainWindowViewModels.cs
@@ -2,40 +2,32 @@
 using Homework9.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace Homework9.ViewModels
 {
     class MainWindowViewModels : ViewModel
     {
+
+        #region Titul
+
         private string _Title = "Домашнее задание 9";
         public string Title
         {
             get => _Title;
             set => Set(ref _Title, value);
         }
-
-        #region Команды
-
-        #region CloseApplicationCommand
-        public ICommand SendText
-        {
-            get;
-        }
-
-        private bool CanSendTextExecute(object p) => true;
-        private void OnSendTextExecuted(object p)
-        {
-            
-        }
-
         #endregion
-        #endregion
+
         public MainWindowViewModels()
         {
-            SendText = new LambdaCommand(OnSendTextExecuted, CanSendTextExecute);
+
+
         }
     }
 }
--- Homework9/Views/Windows/MainWindow.xaml.cs
+++ Homework9/Views/Windows/MainWindow.xaml.cs
@@ -4,10 +4,15 @@ namespace Homework9
 {
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
     }
 }
