ebbdd4e03da302ea087a186788dad8b52ae243d0
EvgeniyIlichev <888ilichoff@mail.ru>
2023-02-01 20:00:31 +0300

    Добавил лямбду команду(которой не хватало в базе) и добавил команду SendText

--- Homework9/ViewModels/MainWindowViewModels.cs
+++ Homework9/ViewModels/MainWindowViewModels.cs
@@ -1,13 +1,41 @@
-﻿using System;
+﻿using Homework9.Infrastructure.Commands;
+using Homework9.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Homework9.ViewModels
 {
-    class MainWindowViewModels
+    class MainWindowViewModels : ViewModel
     {
+        private string _Title = "Домашнее задание 9";
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        #region Команды
+
+        #region CloseApplicationCommand
+        public ICommand SendText
+        {
+            get;
+        }
+
+        private bool CanSendTextExecute(object p) => true;
+        private void OnSendTextExecuted(object p)
+        {
+            
+        }
+
+        #endregion
+        #endregion
         public MainWindowViewModels()
         {
+            SendText = new LambdaCommand(OnSendTextExecuted, CanSendTextExecute);
         }
     }
 }
--- Homework9/Views/Windows/MainWindow.xaml.cs
+++ Homework9/Views/Windows/MainWindow.xaml.cs
@@ -8,5 +8,6 @@ namespace Homework9
         {
             InitializeComponent();
         }
+
     }
 }
